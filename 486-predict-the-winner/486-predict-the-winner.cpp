29a2ae31edadc78871711e4160337c93668c421f
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-03-30 16:04:34 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (96.82%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        int dp[n][n][2];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<n;i++) {dp[i][i][0]=nums[i];dp[i][i][1]=-nums[i];}
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                dp[i][j][0]=max(dp[i][j-1][1]+nums[j],dp[i+1][j][1]+nums[i]);
+                dp[i][j][1]=min(dp[i][j-1][0]-nums[j],dp[i+1][j][0]-nums[i]);
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++) sum+=nums[i];
+        int twoplayer=sum+dp[0][n-1][0];
+        if(twoplayer>=2*sum-twoplayer) return true;
+        return false;
+    }
+};
