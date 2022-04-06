4e6db60c59f94f152bc20e100eed5d369d69511c
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-04-06 14:33:42 +0530

    Time: 705 ms (37.08%), Space: 10.4 MB (90.82%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n=arr.size();
+        int mp[101];
+        memset(mp,0,sizeof(mp));
+        int ans=0;
+        int mod=1e9+7;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(target-arr[i]-arr[j]>=0&&target-arr[i]-arr[j]<=100) ans=(ans+mp[target-arr[i]-arr[j]])%mod;
+            }
+            mp[arr[i]]++;
+        }
+        return ans;
+    }
+};
