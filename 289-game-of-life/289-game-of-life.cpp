885b7f1642a84de38d6d6e44d3a26bc13c4b8aa8
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-04-12 18:24:22 +0530

    Time: 6 ms (17.56%), Space: 6.8 MB (84.44%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -5,26 +5,30 @@ public:
     void gameOfLife(vector<vector<int>>& board) {
         int n=board.size();
         int m=board[0].size();
-        map<pair<int,int>,int>mp;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                int cnt=0;
                 for(int p=0;p<8;p++){
                     int ni=i+nx[p];
                     int nj=j+ny[p];
-                    if(ni>=0&&ni<n&&nj>=0&&nj<m&&board[ni][nj]==1) mp[{i,j}]++;
+                    if(ni>=0&&nj>=0&&ni<n&&nj<m){
+                        if(board[ni][nj]==1||board[ni][nj]==2) cnt++;
+                    } 
+                }
+                if(board[i][j]==1){
+                    if(cnt<2) board[i][j]=2;
+                    else if(cnt>3) board[i][j]=2;
+                }
+                else{
+                    if(cnt==3) board[i][j]=3;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]==1){
-                    if(mp[{i,j}]<2) board[i][j]=0;
-                    else if(mp[{i,j}]>3) board[i][j]=0; 
-                }
-                else{
-                    if(mp[{i,j}]==3) board[i][j]=1;
-                }
+                if(board[i][j]==2) board[i][j]=0;
+                else if(board[i][j]==3) board[i][j]=1;
             }
         }
     }
-};  
+};
