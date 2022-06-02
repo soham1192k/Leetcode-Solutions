87171714ade66c359f1a065640439ee854173922
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-06-02 19:22:35 +0530

    Time: 657 ms (12.79%), Space: 158.6 MB (88.76%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -5,12 +5,9 @@ public:
         vector<pair<int,int>>v(n);
         for(int i=0;i<n;i++) v[i]={growTime[i],plantTime[i]};
         sort(v.rbegin(),v.rend());
-        priority_queue<pair<int,int>>q;
-        for(int i=0;i<n;i++) q.push(v[i]);
         int plantday=0,days=0;
-        while(!q.empty()){
-            pair<int,int>curr=q.top();
-            q.pop();
+        for(int i=0;i<n;i++){
+            pair<int,int>curr=v[i];
             days=max(days,plantday+curr.first+curr.second);
             plantday+=curr.second;
         }
