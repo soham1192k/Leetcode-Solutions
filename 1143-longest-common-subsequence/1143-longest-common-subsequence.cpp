91ecbf7f41c4bd88a702e328070807cd05b01536
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-06-08 22:28:51 +0530

    Time: 19 ms (76.08%), Space: 10.6 MB (87.86%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string s, string t) {
+        int n=s.length();
+        int m=t.length();
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        } 
+        return dp[n][m];
+    }
+};
