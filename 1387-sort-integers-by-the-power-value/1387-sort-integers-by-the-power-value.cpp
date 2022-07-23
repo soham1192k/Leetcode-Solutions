6bc25d85acf221bd599f6c66201eef3f6c8b0129
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-07-23 16:17:24 +0530

    Time: 248 ms (30.40%), Space: 50.3 MB (15.32%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    map<int,int>dp;
     int compute(int x){
         if(x==1) return 0;
+        if(dp.find(x)!=dp.end()) return dp[x]; 
         int ans=0;
         if(x%2) ans=1+compute(3*x+1);
         else ans=1+compute(x/2);
-        return ans;
+        return dp[x]=ans;
     }
     int getKth(int lo, int hi, int k) {
         vector<pair<int,int>>hold;
