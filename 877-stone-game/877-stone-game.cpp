a0bde3712a17610748c525966a473c9bce16733c
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-08-05 19:56:48 +0530

    Time: 33 ms (29.13%), Space: 8.6 MB (42.20%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -4,18 +4,12 @@ public:
     int func(vector<int>&piles,int i,int j){
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        bool alice_chance=false;
-        if((j-i)%2) alice_chance=true;
-        int ans=0;
-        if(alice_chance) ans=max(piles[i]+func(piles,i+1,j),piles[j]+func(piles,i,j-1));
-        else ans=min(-piles[i]+func(piles,i+1,j),-piles[j]+func(piles,i,j-1));
-        return dp[i][j]=ans;
+        return dp[i][j]=max(piles[i]-func(piles,i+1,j),piles[j]-func(piles,i,j-1));
     }
     bool stoneGame(vector<int>& piles) {
         memset(dp,-1,sizeof(dp));
         int n=piles.size();
-        int val=func(piles,0,n-1);
-        if(val>0) return true;
+        if(func(piles,0,n-1)>0) return true;
         return false;
     }
 };
