218a5d5d166551f48d3972d149d791859c57e3ca
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-08-05 14:33:55 +0530

    Time: 15 ms (82.63%), Space: 12.1 MB (51.10%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>>ans;
     void dfs(int src,vector<vector<int>>&graph,int n,vector<int>&path){
         path.push_back(src);
-        if(src==n-1) {ans.push_back(path);path.pop_back();return;}
+        if(src==n-1) ans.push_back(path);
         for(auto edge:graph[src]) dfs(edge,graph,n,path);
         path.pop_back();
     }
