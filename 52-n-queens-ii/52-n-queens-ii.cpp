9b9d34df1929a6bdabc654e86a21bd7ca0483952
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-08-10 01:16:43 +0530

    Time: 4 ms (77.46%), Space: 5.8 MB (97.43%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
-    bool visited[10][10];
-    int ans;
-    bool ok(int i,int j,int n){
+    vector<vector<string>>ans;
+    int cnt;
+    bool ok(int i,int j,int n,vector<string>&hold){
         int k=0,l=0;
         for(k=0;k<n;k++){
-            if(visited[k][j]) return false;
+            if(hold[k][j]=='Q') return false;
         }
         for(l=0;l<n;l++){
-            if(visited[i][l]) return false;
+            if(hold[i][l]=='Q') return false;
         }
         for(k=i,l=j;k<n&&l>=0;k++,l--){
-            if(visited[k][l]) return false;
+            if(hold[k][l]=='Q') return false;
         }
         for(k=i,l=j;k>=0&&l<n;k--,l++){
-            if(visited[k][l]) return false;
+            if(hold[k][l]=='Q') return false;
         }
         for(k=i,l=j;k>=0&&l>=0;k--,l--){
-            if(visited[k][l]) return false;
+            if(hold[k][l]=='Q') return false;
         }
         for(k=i,l=j;k<n&&l<n;k++,l++){
-            if(visited[k][l]) return false;
+            if(hold[k][l]=='Q') return false;
         }
         return true;
     }
-    void dfs(int row,int n){
-        if(row==n) {ans++;return;}
-        for(int col=0;col<n;col++){
-            if(ok(row,col,n)){
-                visited[row][col]=true;
-                dfs(row+1,n);
-                visited[row][col]=false;
-            }
+    void dfs(int row,int n,vector<string>&hold){
+        if(row==n){
+            ans.push_back(hold);
+            return;
         }
+        for(int col=0;col<n;col++){
+            if(ok(row,col,n,hold)){
+                hold[row][col]='Q';
+                dfs(row+1,n,hold);
+                hold[row][col]='.';
+            }
+        }   
     }
-    int totalNQueens(int n) {
-        dfs(0,n);
+    vector<vector<string>> solveNQueens(int n) {
+        string s="";
+        for(int i=1;i<=n;i++) s+='.';
+        vector<string>hold;
+        for(int i=0;i<n;i++) hold.push_back(s);
+        dfs(0,n,hold);
         return ans;
     }
 };
