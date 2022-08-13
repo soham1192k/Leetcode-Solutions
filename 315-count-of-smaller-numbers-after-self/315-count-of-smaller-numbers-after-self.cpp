cddc3bd8582014b7c256dd2f7fecb40e749e7aed
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-08-14 02:13:19 +0530

    Time: 1172 ms (25.06%), Space: 249 MB (15.25%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    int st[100005];
-    void update(int si,int ss,int se,int idx){
-        if(ss==se){
-            st[si]++;
-            return;
+    void merge(vector<pair<int,int>>&v,int left,int mid,int right,vector<int>&count) {
+        vector<pair<int,int>>tmp(right-left+1);
+        int i = left;
+        int j = mid+1;
+        int k = 0;
+        while (i<=mid&&j<=right){
+            if(v[i].first<=v[j].first) tmp[k++]=v[j++];
+            else{
+                count[v[i].second]+=right-j+1;
+                tmp[k++]=v[i++];
+            }
         }
-        int mid=(ss+se)>>1;
-        if(idx<=mid) update(2*si+1,ss,mid,idx);
-        else update(2*si+2,mid+1,se,idx);
-        st[si]=st[2*si+1]+st[2*si+2];
-    }
-    int query(int si,int ss,int se,int qs,int qe){
-        if(qs>se||qe<ss) return 0;
-        if(qs<=ss&&qe>=se) return st[si];
-        int mid=(ss+se)>>1;
-        return query(2*si+1,ss,mid,qs,qe)+query(2*si+2,mid+1,se,qs,qe);
+        while(i<=mid) tmp[k++]=v[i++];
+        while(j<=right) tmp[k++]=v[j++];
+        for(int i=left;i<=right;i++) v[i]=tmp[i-left];
+    }  
+    void mergesort(vector<pair<int,int>>&nums,int low,int high,vector<int>&ans){
+        if(low>=high) return;
+        int mid=(low+high)>>1;
+        mergesort(nums,low,mid,ans);
+        mergesort(nums,mid+1,high,ans);
+        merge(nums,low,mid,high,ans);
     }
     vector<int> countSmaller(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n;i++) nums[i]+=10000;
-        vector<int>ans;
-        for(int i=n-1;i>=0;i--){
-            ans.push_back(query(0,0,20000,0,nums[i]-1));
-            update(0,0,20000,nums[i]);
-        }
-        reverse(ans.begin(),ans.end());
+        vector<int>ans(n,0);
+        vector<pair<int,int>>hold;
+        for(int i=0;i<n;i++) hold.push_back({nums[i],i});
+        mergesort(hold,0,n-1,ans);
         return ans;
     }
 };
