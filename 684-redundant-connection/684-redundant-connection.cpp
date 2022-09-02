54344ebc7d92e08b3bfda038d4852adcc0c1263a
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2022-09-02 14:14:00 +0530

    Time: 16 ms (37.93%), Space: 8.7 MB (87.29%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int par[1001];
-    int sz[1001];
-    int find(int x){
-        if(par[x]==x) return x;
-        return par[x]=find(par[x]);
+    int par[1001],sz[1001];
+    int find_set(int x){
+        if(x==par[x]) return x;
+        return par[x]=find_set(par[x]);
     }
     void merge(int a,int b){
-        a=find(a);
-        b=find(b);
+        a=find_set(a);
+        b=find_set(b);
         if(a!=b){
             if(sz[a]<sz[b]) swap(a,b);
             par[b]=a;
@@ -17,12 +16,15 @@ public:
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        for(int i=1;i<=n;i++) {par[i]=i;sz[i]=1;}
-        for(int i=0;i<n;i++){
-            int a=find(edges[i][0]);
-            int b=find(edges[i][1]);
-            if(a==b) return {edges[i][0],edges[i][1]};
-            else merge(edges[i][0],edges[i][1]);
+        for(int i=1;i<=n;i++){
+            par[i]=i;
+            sz[i]=1;
+        } 
+        for(auto x:edges){
+            int a=find_set(x[0]);
+            int b=find_set(x[1]);
+            if(a!=b) merge(x[0],x[1]);
+            else return {x[0],x[1]};
         }
         return {};
     }
